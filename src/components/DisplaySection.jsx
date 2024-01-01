5a204095bf50e50609447b3e15990ad2f8726acc
Aditya <aditya.chaudhary1558@gmail.com>
2024-01-01 15:52:37 +0545

    add the display-section in the website

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function DisplaySection(){
+    return(
+        <div className="display-section wrapper">
+            <h2 className="title">New</h2>
+            <p className="text">Brilliant. </p>
+            <span className="description">A display that's upto 2x brighter in the sun.</span>
+            <button className="button">Try me! </button>
+            <button className="back-button">TOP</button>
+        </div>
+        
+    )
+}
+
+
+export default DisplaySection
