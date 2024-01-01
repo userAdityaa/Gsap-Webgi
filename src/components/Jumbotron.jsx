5692ef93bb363726f36e6fbf38d224818869c294
Aditya <aditya.chaudhary1558@gmail.com>
2024-01-01 15:44:01 +0545

    made the sound section scroll-page view

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,8 +3,19 @@ import Iphone from '../assets/images/iphone-14.jpg';
 import HoldingIphone from '../assets/images/iphone-hand.png';
 
 function Jumbotron(){
+
+    const handleSoundMore = () => {
+        const element = document.querySelector('.sound-section');
+        window.scrollTo({
+            top: element?.getBoundingClientRect().top,
+            left: 0, 
+            behavior: 'smooth'
+        })
+    }
+
+
     return(
-        <div className="jumbotron-section wrapper">
+        <div className="jumbotron-section wrapper"> 
             <h2 className="title">New</h2>
             <img src={Iphone} alt="iphone 14 pro" />
             <p className="text">Big and bigger.</p>
@@ -14,7 +25,7 @@ function Jumbotron(){
                     <button className="button">Buy</button>
                 </li>
                 <li>
-                    <a className="link">Learn More</a>
+                    <a className="link" onClick={handleSoundMore}>Learn More</a>
                 </li>
             </ul>
             <img src={HoldingIphone} alt="iphone " className="iphone-img" />
